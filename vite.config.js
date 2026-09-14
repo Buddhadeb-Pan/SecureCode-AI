d139be521593d9a48cedf4db4a9a269e2663e4e1
Buddhadeb-Pan <panbuddhadeb230@gmail.com>
2026-09-15 00:35:18 +0530

    Update GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/SourceCode_AI/",
+    base: "/SecureCode-AI/",
     plugins: [react()],
 })
